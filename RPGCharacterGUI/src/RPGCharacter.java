0723590b66e715c1048b820df9f413cabe9afbe2
jlaw <jlaw@warren.k12.in.us>
2021-10-27 07:42:15 -0400

    Initial commit

---
+++ RPGCharacterGUI/src/RPGCharacter.java
@@ -0,0 +1,8 @@
+public class RPGCharacter {
+
+    /*
+    Make a GUI for your RPG Character builder you made in the last Unit. The user should enter primary stats in input box and the GUI
+    should display the character's battle stats. The GUI should have at least 4 label, 4 inputs and a button to click which calculates and displays
+    the battle stats.
+     */
+}
---
+++ WidgetFactory/src/WidgetFactory.java
@@ -0,0 +1,31 @@
+public class WidgetFactory {
+
+    /*
+    Lessons L1 - L3
+
+    You have been asked by the manager of the Widget Factory to create a program which will tell how many days it will take to produce a number of widgets.
+    It should also output the cost of the widgets, the cost to produce the widget and the profit.
+
+    Specific Details:
+     - 10 Widgets are made each hour
+     - Plant runs 2 shifts of 8 hours a day, requiring 5 workers a shift
+     - each worker makes $16.50 an hour
+     - each widget sells for $10
+
+     Programming Details:
+     - prompt the user for the number of widgets using pop-up boxes
+     - display # of widgets, # of days, Cost of production, Cost of widgets and profit
+     - 2 or more (preferrably more) methods - at least 1 to calculate # of day and one to calculate production costs
+     - only WHOLE number of days, no half or partial days.
+     - Output should be in proper currency form
+
+     Example Run:
+
+     Number of Widgets: 1000
+     # of Days: 7
+     Cost of Widgets: $10,000.00
+     Cost of Production: $9,240.00
+     Profit: $760.00
+
+     */
+}
