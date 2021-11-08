0d343f1869322f2b7ba0b6154f23c7c5a9cae04f
Tony Young <youngton000@warren.k12.in.us>
2021-11-08 11:47:28 -0500

    not done at all

--- RPGCharacterGUI/src/RPGCharacter.java
+++ RPGCharacterGUI/src/RPGCharacter.java
@@ -5,4 +5,8 @@ public class RPGCharacter {
     should display the character's battle stats. The GUI should have at least 4 label, 4 inputs and a button to click which calculates and displays
     the battle stats.
      */
+    public static void main(String[] args) {
+        new RPGCharacterGUI();
+
+    }
 }
--- RPGCharacterGUI/src/RPGCharacterGUI.java
+++ RPGCharacterGUI/src/RPGCharacterGUI.java
@@ -1,2 +1,25 @@
+import javax.swing.*;
+import java.awt.*;
+
+
 public class RPGCharacterGUI {
+
+    JFrame frame;
+
+
+
+
+
+    public void RPGCharacterGUI() {
+        frame = new JFrame("RPG Character");
+        frame.setSize(400, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+
+        JPanel panel = new Character();
+
+        frame.add(panel);
+        frame.setVisible(true);
+
+    }
 }
